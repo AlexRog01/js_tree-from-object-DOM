74bc427e236614298e723cf30cad47c43608b0ea
Alex Rog <sasharg303@gmail.com>
2023-11-22 14:18:39 +0100

    make build

---
+++ index.4210a8ce.js
@@ -0,0 +1,2 @@
+!function e(l,n){for(let t in l.insertAdjacentHTML("beforeend","<ul></ul>"),n){l.lastElementChild.insertAdjacentHTML("beforeend",`<li>${t}</li>`);let r=l.lastElementChild.lastElementChild;Object.keys(n[t]).length>0&&e(r,n[t])}}(document.querySelector("#tree"),{Drink:{Wine:{},Schnaps:{}},Fruit:{Red:{Cherry:{},Strawberry:{}},Yellow:{Banana:{},Pineapple:{}}}});
+//# sourceMappingURL=index.4210a8ce.js.map
